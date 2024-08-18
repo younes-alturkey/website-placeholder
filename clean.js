e3885b51feb1aca044a55a5214772367e1137486
Younes Alturkey <me@younes.expert>
2024-08-18 21:49:52 +0300

    latest

--- clean.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process")
-
-exec('find . -name ".DS_Store" -exec rm {} \\;', (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Error: ${err}`)
-    return
-  }
-  console.log("All .DS_Store files deleted successfully from all folders.")
-})
